c40ea7c776b162cb0b15297c9ceb25e272b3bb3b
Loc Tran <mrlocspoc@gmail.com>
2024-01-03 23:17:02 +0700

    5. Tailwind installation and setup complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Button, ConfigProvider } from 'antd';
 
 function App() {
 	return (
-		<div>
+		<div className="flex justify-center h-screen items-center flex-col bg-red-500">
 			<h1>DM Tracker</h1>
 			<Button type="primary">Button</Button>
 		</div>
