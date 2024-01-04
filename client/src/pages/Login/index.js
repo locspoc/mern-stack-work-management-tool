12cc22d5f2bd7d3fe9a700f57729b1f99e187771
Loc Tran <mrlocspoc@gmail.com>
2024-01-04 16:52:32 +0700

    7. Login screen complete

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
+
+import Divider from '../../components/Divider';
 
 function Login() {
-	return <div>Login</div>;
+	const onFinish = (values) => {
+		console.log('Success: ', values);
+	};
+	return (
+		<div className="grid grid-cols-2">
+			<div className="bg-primary h-screen"></div>
+			<div className="flex justify-center items-center">
+				<div className="w-[400px]">
+					<h1 className="text-2xl text-gray-700">
+						LOGIN TO YOUR ACCOUNT
+					</h1>
+					<Divider />
+					<Form layout="vertical" onFinish={onFinish}>
+						<Form.Item label="Email" name="email">
+							<Input />
+						</Form.Item>
+						<Form.Item label="Password" name="password">
+							<Input type="password" />
+						</Form.Item>
+						<Button type="primary" htmlType="submit" block>
+							Login
+						</Button>
+						<div className="flex justify-center mt-5">
+							<span>
+								Don't have an account?{' '}
+								<Link to="/register">Register</Link>
+							</span>
+						</div>
+					</Form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
